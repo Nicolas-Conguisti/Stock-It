7a49d6e664c3e8fee37eef326c01059d5963abeb
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2024-09-22 20:28:47 +0200

    Création du VO d'un objet avec le attributs

---
+++ src/Database/DATABASE_INSERTION.sql
@@ -0,0 +1,59 @@
+INSERT INTO objet (
+    OBJET_ID,
+    OBJET_NOM,
+    OBJET_MARQUE,
+    OBJET_REFERENCE,
+    OBJET_PRIX,
+    OBJET_NOMBRE,
+    OBJET_QUANTITE,
+    OBJET_UTILISATION,
+    OBJET_LONGUEUR,
+    OBJET_LARGEUR,
+    OBJET_HAUTEUR,
+    OBJET_ID_CONTENANT,
+    LIEUSTOCK_ID
+) VALUES (
+    1,                   
+    'Table',        
+    'BUT',     
+    'Table blanche 0442',  
+    100.00,                   
+    1,  
+    NULL,
+    'Placement dans la cuisine',
+    102.80,                       
+    55.3,                        
+    90.9,                       
+    NULL,                   
+    1
+);
+
+INSERT INTO objet (
+    OBJET_ID,
+    OBJET_NOM,
+    OBJET_MARQUE,
+    OBJET_REFERENCE,
+    OBJET_PRIX,
+    OBJET_NOMBRE,
+    OBJET_QUANTITE,
+    OBJET_UTILISATION,
+    OBJET_LONGUEUR,
+    OBJET_LARGEUR,
+    OBJET_HAUTEUR,
+    OBJET_ID_CONTENANT,
+    LIEUSTOCK_ID
+) VALUES (
+    2,                   
+    'TV',        
+    'Philips',     
+    'TV Noire 1080p Philips Z3',  
+    180.00,                   
+    1,  
+    NULL,
+    'Placement dans le salon',
+    NULL,                       
+    NULL,                        
+    NULL,                       
+    NULL,                   
+    1
+);
--- src/main/java/com/sit/Stock_it/Data/ObjetVO.java
+++ src/main/java/com/sit/Stock_it/Data/ObjetVO.java
@@ -6,32 +6,164 @@ import jakarta.persistence.*;
 @Table(name = "OBJET")
 public class ObjetVO {
 
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private long id;
+    //ATTRIBUTS
 
-    @Column(name = "Nom")
-    private String nom;
+    @Id
+    @Column(name = "OBJET_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long OBJET_ID;
+
+    @Column(name = "OBJET_NOM", nullable = false)
+    private String OBJET_NOM;
+
+    @Column(name = "OBJET_MARQUE")
+    private String OBJET_MARQUE;
+
+    @Column(name = "OBJET_REFERENCE")
+    private String OBJET_REFERENCE;
+
+    @Column(name = "OBJET_PRIX")
+    private Long OBJET_PRIX;
+
+    @Column(name = "OBJET_NOMBRE")
+    private Integer OBJET_NOMBRE;
+
+    @Column(name = "OBJET_QUANTITE")
+    private String OBJET_QUANTITE;
+
+    @Column(name = "OBJET_UTILISATION")
+    private String OBJET_UTILISATION;
+
+    @Column(name = "OBJET_LONGUEUR")
+    private Integer OBJET_LONGUEUR;
+
+    @Column(name = "OBJET_LARGEUR")
+    private Integer OBJET_LARGEUR;
+
+    @Column(name = "OBJET_HAUTEUR")
+    private Integer OBJET_HAUTEUR;
+
+    @Column(name = "OBJET_ID_CONTENANT")
+    private Long OBJET_ID_CONTENANT;
+
+    @Column(name = "LIEUSTOCK_ID", nullable = false)
+    //METTRE JOIN
+    private Integer LIEUSTOCK_ID;
+
+
+//CONSTRUCTEURS
 
     public ObjetVO(String nom) {
-        this.nom = nom;
+        this.OBJET_NOM = nom;
     }
 
     public ObjetVO() {
     }
 
-    public long getId() {
-        return id;
+    //GETTERS ET SETTERS
+
+    public Integer getOBJET_LARGEUR() {
+        return OBJET_LARGEUR;
     }
 
-    public void setId(long id) {this.id = id;}
-
-    public String getNom() {
-        return nom;
+    public void setOBJET_LARGEUR(Integer OBJET_LARGEUR) {
+        this.OBJET_LARGEUR = OBJET_LARGEUR;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+    public Integer getOBJET_LONGUEUR() {
+        return OBJET_LONGUEUR;
     }
+
+    public void setOBJET_LONGUEUR(Integer OBJET_LONGUEUR) {
+        this.OBJET_LONGUEUR = OBJET_LONGUEUR;
+    }
+
+    public String getOBJET_UTILISATION() {
+        return OBJET_UTILISATION;
+    }
+
+    public void setOBJET_UTILISATION(String OBJET_UTILISATION) {
+        this.OBJET_UTILISATION = OBJET_UTILISATION;
+    }
+
+    public Integer getOBJET_HAUTEUR() {
+        return OBJET_HAUTEUR;
+    }
+
+    public void setOBJET_HAUTEUR(Integer OBJET_HAUTEUR) {
+        this.OBJET_HAUTEUR = OBJET_HAUTEUR;
+    }
+
+    public Long getOBJET_ID_CONTENANT() {
+        return OBJET_ID_CONTENANT;
+    }
+
+    public void setOBJET_ID_CONTENANT(Long OBJET_ID_CONTENANT) {
+        this.OBJET_ID_CONTENANT = OBJET_ID_CONTENANT;
+    }
+
+    public String getOBJET_QUANTITE() {
+        return OBJET_QUANTITE;
+    }
+
+    public void setOBJET_QUANTITE(String OBJET_QUANTITE) {
+        this.OBJET_QUANTITE = OBJET_QUANTITE;
+    }
+
+    public Integer getOBJET_NOMBRE() {
+        return OBJET_NOMBRE;
+    }
+
+    public void setOBJET_NOMBRE(Integer OBJET_NOMBRE) {
+        this.OBJET_NOMBRE = OBJET_NOMBRE;
+    }
+
+    public Long getOBJET_PRIX() {
+        return OBJET_PRIX;
+    }
+
+    public void setOBJET_PRIX(Long OBJET_PRIX) {
+        this.OBJET_PRIX = OBJET_PRIX;
+    }
+
+    public String getOBJET_REFERENCE() {
+        return OBJET_REFERENCE;
+    }
+
+    public void setOBJET_REFERENCE(String OBJET_REFERENCE) {
+        this.OBJET_REFERENCE = OBJET_REFERENCE;
+    }
+
+    public String getOBJET_MARQUE() {
+        return OBJET_MARQUE;
+    }
+
+    public void setOBJET_MARQUE(String OBJET_MARQUE) {
+        this.OBJET_MARQUE = OBJET_MARQUE;
+    }
+
+    public String getOBJET_NOM() {
+        return OBJET_NOM;
+    }
+
+    public void setOBJET_NOM(String OBJET_NOM) {
+        this.OBJET_NOM = OBJET_NOM;
+    }
+
+    public Long getOBJET_ID() {
+        return OBJET_ID;
+    }
+
+    public void setOBJET_ID(Long OBJET_ID) {
+        this.OBJET_ID = OBJET_ID;
+    }
+
+    public Integer getLIEUSTOCK_ID() {
+        return LIEUSTOCK_ID;
+    }
+
+    public void setLIEUSTOCK_ID(Integer LIEUSTOCK_ID) {
+        this.LIEUSTOCK_ID = LIEUSTOCK_ID;
+    }
+
 }
